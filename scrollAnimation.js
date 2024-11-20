28ec3ed9e529b78f0e1e0aaf36f2d49a0b63360c
Gideon <codingweb656@gmail.com>
2024-11-20 09:20:27 +0000

    Updated the reveal on scroll

--- scrollAnimation.js
+++ scrollAnimation.js
@@ -19,7 +19,7 @@ ScrollReveal().reveal(".hero-texts", {
   distance: "50%",
   origin: "left",
   duration: 2000,
-  delay: 500,
+  delay: 200,
 });
 
 //Hero Image
@@ -33,14 +33,12 @@ ScrollReveal().reveal(".hero-img", {
 ScrollReveal().reveal(".about-img", {
   opacity: 0,
   duration: 2000,
-  delay: 500,
 });
 
 //About Divs
 ScrollReveal().reveal(aboutDivs, {
   opacity: 0,
   duration: 2000,
-  delay: 500,
 });
 
 //Services Image
@@ -48,54 +46,52 @@ ScrollReveal().reveal(".services-img", {
   distance: "100%",
   origin: "left",
   duration: 2000,
-  delay: 500,
 });
 
 //Services Text
 ScrollReveal().reveal(".services-text", {
   distance: "100%",
   origin: "right",
-  duration: 2500,
-  delay: 500,
+  duration: 2000,
+  delay: 200,
 });
 
 //Portfolio Heading 2
 ScrollReveal().reveal(".portfolio-h2", {
   scale: 0,
   duration: 2000,
-  delay: 500,
 });
 
 //Portfolio-1
 ScrollReveal().reveal(".portfolio-1", {
   distance: "150%",
   origin: "bottom",
-  duration: 500,
-  delay: 500,
+  duration: 2000,
+  delay: 100,
 });
 
 //Portfolio-2
 ScrollReveal().reveal(".portfolio-2", {
   distance: "150%",
   origin: "bottom",
-  duration: 500,
-  delay: 600,
+  duration: 2000,
+  delay: 200,
 });
 
 //Portfolio-3
 ScrollReveal().reveal(".portfolio-3", {
   distance: "150%",
   origin: "bottom",
-  duration: 500,
-  delay: 700,
+  duration: 2000,
+  delay: 300,
 });
 
 //Portfolio-4
 ScrollReveal().reveal(".portfolio-4", {
   distance: "150%",
   origin: "bottom",
-  duration: 500,
-  delay: 800,
+  duration: 2000,
+  delay: 400,
 });
 
 //Blog Head
@@ -103,7 +99,6 @@ ScrollReveal().reveal(".blog-head", {
   distance: "100%",
   origin: "top",
   duration: 2000,
-  delay: 500,
 });
 
 //Blog Details
@@ -111,7 +106,6 @@ ScrollReveal().reveal(".blog-details", {
   distance: "100%",
   origin: "bottom",
   duration: 2000,
-  delay: 500,
 });
 
 //Message Text
@@ -119,7 +113,7 @@ ScrollReveal().reveal(".message-text", {
   distance: "100%",
   origin: "left",
   duration: 2000,
-  delay: 700,
+  delay: 200,
 });
 
 //Message Form
@@ -127,12 +121,10 @@ ScrollReveal().reveal(".message-form", {
   distance: "100%",
   origin: "right",
   duration: 2000,
-  delay: 500,
 });
 
 //Footer
 ScrollReveal().reveal(".footer", {
   opacity: 0,
   duration: 2000,
-  delay: 500,
 });
